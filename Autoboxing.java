72450e3d317b70573443681faee940a2fb7540ce
Mayur <mayurrathore.work@gmail.com>
2020-12-01 07:56:43 +0530

    Wrapper class concepts

---
+++ Autoboxing.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Autoboxing {
+
+    public static void main(String[] args){
+
+        char ch = 'a';
+
+        // Autoboxing- primitive to Character object conversion
+        Character a = ch;
+
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+        // Autoboxing because ArrayList stores only objects
+        arrayList.add(25);
+
+        System.out.println(arrayList.get(0));
+    }
+}
